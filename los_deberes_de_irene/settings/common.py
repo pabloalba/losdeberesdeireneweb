4627380f784b9cadfd04ed751b33a54c4b71122a
Andrés Moya <andres.moya@kaleidos.net>
2021-07-19 11:32:31 +0200

    Add skeleton of home view and styles

--- los_deberes_de_irene/settings/common.py
+++ los_deberes_de_irene/settings/common.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'los_deberes_de_irene',
     'homework',
 ]
 
--- los_deberes_de_irene/urls.py
+++ los_deberes_de_irene/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path
+
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r"^$", views.HomeView.as_view(), name='home'),
 ]
