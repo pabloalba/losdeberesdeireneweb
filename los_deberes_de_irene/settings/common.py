7d798bf8fab0415ff27d7affbaf06aae9f66ba97
Andrés Moya <andres.moya@kaleidos.net>
2021-07-19 15:39:46 +0200

    Add initial splash screen

--- los_deberes_de_irene/settings/common.py
+++ los_deberes_de_irene/settings/common.py
@@ -119,12 +119,12 @@ PWA_APP_ICONS_APPLE = [
         'sizes': '289x289'
     }
 ]
-# PWA_APP_SPLASH_SCREEN = [
-#     {
-#         'src': '/static/images/icons/splash-640x1136.png',
-#         'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
-#     }
-# ]
+PWA_APP_SPLASH_SCREEN = [
+    {
+        'src': '/static/img/splash.png',
+        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+    }
+]
 PWA_APP_DIR = 'ltr'
 PWA_APP_LANG = 'es-ES'
 PWA_APP_DEBUG_MODE = True
