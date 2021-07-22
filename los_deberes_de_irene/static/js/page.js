26fe4b78322d99bfa4120b5daf12aca69a85de68
Andrés Moya <andres.moya@kaleidos.net>
2021-07-22 13:04:02 +0200

    Add label editor

--- los_deberes_de_irene/static/js/page.js
+++ los_deberes_de_irene/static/js/page.js
@@ -12,7 +12,13 @@ let scrollOffset = 0;
 let viewPortElement;
 let svgElement;
 let labelsElement;
+let labelInputElement;
 
+let labelInputX;
+let labelInputY;
+
+
+// ====== Initialization ==================
 
 export function setupPage(width, height) {
   imageWidth = width;
@@ -28,19 +34,26 @@ export function setupPage(width, height) {
   }
 }
 
+
 function pageLoaded() {
   viewPortElement = document.getElementById("page-viewport");
   svgElement = viewPortElement.querySelectorAll("svg")[0];
   labelsElement = document.getElementById("labels");
+  labelInputElement = document.getElementById("label-input");
 
   window.addEventListener("resize", onResize);
   svgElement.addEventListener("wheel", onWheel);
   svgElement.addEventListener("click", onClick);
 
+  labelInputElement.addEventListener("blur", onBlur);
+
   // The resize event does not occur in the initial load,
   // so we call it once here.
   onResize();
 }
+
+
+// ====== Events ==================
 
 function onResize() {
   // On window resize, set the viewport size to ocuppy all the available
@@ -70,6 +83,30 @@ function onWheel(event) {
 }
 
 
+function onClick(event) {
+  event.preventDefault();
+
+  if (viewPortElement) {
+    labelInputX = event.offsetX;
+    labelInputY = event.offsetY;
+    showLabelInput();
+  }
+}
+
+
+function onBlur(event) {
+  if (viewPortElement) {
+    const x = screenToImage(labelInputX);
+    const y = screenToImage(labelInputY) + scrollOffset;
+    const text = labelInputElement.value;
+    hideLabelInput();
+    createLabel(x, y, text);
+  }
+}
+
+
+// ====== Auxiliary functions ==================
+
 function setViewBox() {
   svgElement.setAttribute("width", viewPortWidth);
   svgElement.setAttribute("height", viewPortHeight);
@@ -80,27 +117,37 @@ function setViewBox() {
 }
 
 
-function onClick(event) {
-  event.preventDefault();
-
-  if (viewPortElement) {
-    const x = screenToImage(event.offsetX);
-    const y = screenToImage(event.offsetY) + scrollOffset;
-
-    const labelElement = document.createElementNS("http://www.w3.org/2000/svg", "text");
-    labelElement.setAttribute("class", "page-label");
-    labelElement.setAttribute("x", x);
-    labelElement.setAttribute("y", y);
-    labelElement.setAttribute("style", "fill: #54C6EB; font-size: 15");
-    labelElement.textContent = "lorem ipsum dolor sit amet";
-
-    labelsElement.appendChild(labelElement);
-  }
+function showLabelInput() {
+  labelInputElement.style.left = labelInputX;
+  labelInputElement.style.top = labelInputY - 37;
+  labelInputElement.style.fontSize = imageToScreen(15).toString() + "px";
+  labelInputElement.focus();
 }
+
+
+function hideLabelInput() {
+  labelInputElement.style.left = -1000;
+  labelInputElement.style.top = -1000;
+  labelInputElement.value = "";
+}
+
+
+function createLabel(x, y, text) {
+  const labelElement = document.createElementNS("http://www.w3.org/2000/svg", "text");
+  labelElement.setAttribute("class", "page-label");
+  labelElement.setAttribute("x", x);
+  labelElement.setAttribute("y", y);
+  labelElement.setAttribute("style", "fill: #54C6EB; font-size: 15");
+  labelElement.textContent = text;
+
+  labelsElement.appendChild(labelElement);
+}
+
 
 function screenToImage(pos) {
   return pos * viewBoxWidth / viewPortWidth;
 }
+
 
 function imageToScreen(pos) {
   return pos * viewPortWidth / viewBoxWidth;
