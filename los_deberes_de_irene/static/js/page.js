3ce29e4c142ae6f6a319e911d96d063413e7c3d8
Andrés Moya <andres.moya@kaleidos.net>
2021-07-22 09:15:24 +0200

    Scroll page with mouse wheel

--- los_deberes_de_irene/static/js/page.js
+++ los_deberes_de_irene/static/js/page.js
@@ -1,36 +1,71 @@
 let imageWidth = 0;
 let imageHeight = 0;
+
+let viewPortWidth = 0;
+let viewPortHeight = 0;
+
+let viewBoxWidth = 0;
+let viewBoxHeight = 0;
+
 let scrollOffset = 0;
+
+let viewPortElement;
+let svgElement;
+
 
 export function setupPage(width, height) {
   imageWidth = width;
   imageHeight = height;
 
-  window.addEventListener("resize", onResize);
-
-  // The resize event does not occur in the initial load. Here
-  // we wait for the document to be fully loaded, and then call
-  // resize function once.
+  // Wait for the document to be fully loaded, and then finish
+  // the setup (or do it immediately if the document is already
+  // loaded when calling this).
   if (document.readyState != "loading") {
-    onResize();
+    pageLoaded();
   } else {
-    document.addEventListener("DOMContentLoaded", onResize);
+    document.addEventListener("DOMContentLoaded", pageLoaded);
   }
 }
 
-export function onResize() {
-  // On window resize, set the viewpor size to ocuppy all the available
+function pageLoaded() {
+  viewPortElement = document.getElementById("page-viewport");
+  svgElement = viewPortElement.querySelectorAll("svg")[0];
+
+  window.addEventListener("resize", onResize);
+  viewPortElement.addEventListener("wheel", onWheel);
+
+  // The resize event does not occur in the initial load,
+  // so we call it once here.
+  onResize();
+}
+
+function onResize() {
+  // On window resize, set the viewport size to ocuppy all the available
   // space, and zoom the image to fit the viewport width.
-  const viewPortElement = document.getElementById("page-viewport");
-  const viewPortWidth = viewPortElement.offsetWidth;
-  const viewPortHeight = viewPortElement.offsetHeight;
+  viewPortWidth = viewPortElement.offsetWidth;
+  viewPortHeight = viewPortElement.offsetHeight;
 
   const ratio = viewPortHeight / viewPortWidth;
 
-  const viewBoxWidth = imageWidth;
-  const viewBoxHeight = imageWidth * ratio;
+  viewBoxWidth = imageWidth;
+  viewBoxHeight = imageWidth * ratio;
 
-  const svgElement = viewPortElement.querySelectorAll("svg")[0];
+  setViewBox();
+}
+
+
+function onWheel(event) {
+  event.preventDefault();
+
+  scrollOffset += event.deltaY;
+  scrollOffset = Math.min(scrollOffset, (imageHeight - viewBoxHeight));
+  scrollOffset = Math.max(scrollOffset, 0);
+
+  setViewBox();
+}
+
+
+function setViewBox() {
   svgElement.setAttribute("width", viewPortWidth);
   svgElement.setAttribute("height", viewPortHeight);
   svgElement.setAttribute("viewBox",
@@ -38,4 +73,3 @@ export function onResize() {
     viewBoxWidth.toString() + " " + 
     viewBoxHeight.toString());
 }
-
